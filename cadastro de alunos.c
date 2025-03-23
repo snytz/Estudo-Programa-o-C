a28183c2e6e2537840bb7079ae05171d596d7a02
banana flavor <121833694+snytz@users.noreply.github.com>
2025-03-23 13:33:22 -0300

    cadastro de alunos.c

--- cadastro de alunos.c
+++ cadastro de alunos.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <string.h>
 
+int main() {
+
+    char nome[50];
+    int matricula;
+    int idade;
+    float altura;
+    
+
+        printf("Olá bem vindo a nossa instituição, para realizar seu cadastro por favor, preencha o formulário!\n");
+        printf("Digite seu nome completo: ");
+        fgets(nome, 50, stdin);
+        nome[strcspn(nome, "\n")] = 0;
+        printf("Digite sua Matricula: ");
+        scanf("%d", &matricula);
+        printf("Digite sua idade: ");
+        scanf("%d", &idade);
+        printf("Digite sua altura: ");
+        scanf("%f", &altura);
+
+        printf("Muito bem seu cadastro foi realizado em nosso portal!\nSeu nome é %s, matricula: %d, possui %d anos e mede %.2f de altura.", nome, matricula, idade, altura);
+
+
+        return 0;
+}
